922539bf8159a3190f3069fa6ece3ba597d04b7d
Andjelko Perisic <andjelko.perisic@gmail.com>
2023-03-24 11:52:14 +0100

    top toolbar and share icon

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,39 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {TopToolbarComponent} from './top-toolbar/top-toolbar.component';
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatIconModule} from "@angular/material/icon";
+import {MatButtonModule} from "@angular/material/button";
+import {ShareComponent} from './top-toolbar/share/share.component';
+import {MatMenuModule} from "@angular/material/menu";
+import {ClipboardModule} from "@angular/cdk/clipboard";
+import {MatSidenavModule} from "@angular/material/sidenav";
+import {MatTooltipModule} from "@angular/material/tooltip";
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopToolbarComponent,
+    ShareComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatMenuModule,
+    ClipboardModule,
+    MatSidenavModule,
+    MatTooltipModule,
+    MatSnackBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
